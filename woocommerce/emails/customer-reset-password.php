da70e3439bb176a97aad9b49f349654af6c27d2f
Igor Chernenko <igorchernenko92@gmail.com>
2021-03-30 20:35:19 +0300

    CHANGE: user name instead of user login in change password woo letter

--- woocommerce/emails/customer-reset-password.php
+++ woocommerce/emails/customer-reset-password.php
@@ -19,6 +19,9 @@ defined( 'ABSPATH' ) || exit;
 
 do_action( 'woocommerce_email_header', $email_heading, $email );
 
+$user = get_user_by('id', $user_id);
+$user_name = $user->display_name ?? $user_login;
+
 ?>
 <table align="center" cellspacing="0" cellpadding="0" border="0" style="width:100%;max-width:600px;border:0">
     <tbody>
@@ -30,12 +33,12 @@ do_action( 'woocommerce_email_header', $email_heading, $email );
                             <td style="font-family:'Open Sans',sans-serif;font-size:14px;line-height:200%;color:#3c5d90"> Здравствуйте, </td>
                         </tr>
                         <tr>
-                            <td style="font-family:Montserrat,sans-serif;font-weight:700;font-size:32px;line-height:130%;letter-spacing:-.05em;padding-bottom:20px;color:#3c5d90"><?php echo $user_login; ?>! </td>
+                            <td style="font-family:Montserrat,sans-serif;font-weight:700;font-size:32px;line-height:130%;letter-spacing:-.05em;padding-bottom:20px;color:#3c5d90"><?php echo  esc_html( $user_name ); ?>! </td>
                         </tr>
                         <tr>
                             <td style="font-family:'Open Sans',sans-serif;font-size:14px;line-height:200%;color:#3c5d90">
                                 <p><?php printf( esc_html__( 'Someone has requested a new password for the following account on %s:', 'woocommerce' ), esc_html( wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES ) ) ); ?></p>
-                                <p><?php printf( esc_html__( 'Username: %s', 'woocommerce' ), esc_html( $user_login ) ); ?></p>
+                                <p><?php printf( esc_html__( 'Username: %s', 'woocommerce' ), esc_html( $user_name ) ); ?></p>
                                 <p><?php esc_html_e( 'If you didn\'t make this request, just ignore this email. If you\'d like to proceed:', 'woocommerce' ); ?></p>
                                 <p>
                                     <a class="link" href="<?php echo esc_url( add_query_arg( array( 'key' => $reset_key, 'id' => $user_id ), wc_get_endpoint_url( 'lost-password', '', wc_get_page_permalink( 'myaccount' ) ) ) ); ?>"><?php ?>
