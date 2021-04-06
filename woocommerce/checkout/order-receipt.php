e40054e856d0d559eb021e81c5529b456e92bbee
Igor Chernenko <igorchernenko92@gmail.com>
2021-04-06 23:26:58 +0300

    TWEAK: order receipt change

--- woocommerce/checkout/order-receipt.php
+++ woocommerce/checkout/order-receipt.php
@@ -22,23 +22,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div class="order_details order-receipt-details">
 	<div class="order order-receipt-item">
-		<div class="order-receipt-item-left"><?php esc_html_e( 'Order number:', 'woocommerce' ); ?></div>
-		<div class="order-receipt-item-right"><strong><?php echo esc_html( $order->get_order_number() ); ?></strong></div>
+		<div class="order-receipt-item-left"><?php esc_html_e( 'Order number:', 'woocommerce' ); ?> <strong><?php echo esc_html( $order->get_order_number() ); ?></strong></div>
+<!--		<div class="order-receipt-item-right"></div>-->
 	</div>
 	<div class="date order-receipt-item">
-		<div class="order-receipt-item-left"><?php esc_html_e( 'Date:', 'woocommerce' ); ?></div>
-		<div class="order-receipt-item-right"><strong><?php echo esc_html( wc_format_datetime( $order->get_date_created() ) ); ?></strong></div>
+		<div class="order-receipt-item-left"><?php esc_html_e( 'Date:', 'woocommerce' ); ?> <strong><?php echo esc_html( wc_format_datetime( $order->get_date_created() ) ); ?></strong></div>
+<!--		<div class="order-receipt-item-right"><strong>--><?php //echo esc_html( wc_format_datetime( $order->get_date_created() ) ); ?><!--</strong></div>-->
 	</div>
-	<div class="total order-receipt-item">
-		<div class="order-receipt-item-left"><?php esc_html_e( 'Total:', 'woocommerce' ); ?></div>
-		<div class="order-receipt-item-right"><strong><?php echo wp_kses_post( $order->get_formatted_order_total() ); ?></strong></div>
-	</div>
-	<?php if ( $order->get_payment_method_title() ) : ?>
-	<div class="method order-receipt-item">
-		<div class="order-receipt-item-left"><?php esc_html_e( 'Payment method:', 'woocommerce' ); ?></div>
-		<div class="order-receipt-item-right"><strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong></div>
-	</div>
-	<?php endif; ?>
+<!--	<div class="total order-receipt-item">-->
+<!--		<div class="order-receipt-item-left">--><?php //esc_html_e( 'Total:', 'woocommerce' ); ?><!--</div>-->
+<!--		<div class="order-receipt-item-right"><strong>--><?php //echo wp_kses_post( $order->get_formatted_order_total() ); ?><!--</strong></div>-->
+<!--	</div>-->
+<!--	--><?php //if ( $order->get_payment_method_title() ) : ?>
+<!--	<div class="method order-receipt-item">-->
+<!--		<div class="order-receipt-item-left">--><?php //esc_html_e( 'Payment method:', 'woocommerce' ); ?><!--</div>-->
+<!--		<div class="order-receipt-item-right"><strong>--><?php //echo wp_kses_post( $order->get_payment_method_title() ); ?><!--</strong></div>-->
+<!--	</div>-->
+<!--	--><?php //endif; ?>
 </div>
 
 <?php do_action( 'woocommerce_receipt_' . $order->get_payment_method(), $order->get_id() ); ?>
