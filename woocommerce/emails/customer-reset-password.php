d39e55876e2359ee20f4f3f4349940ac1f06d55b
Igor Chernenko <igorchernenko92@gmail.com>
2021-04-07 00:00:05 +0300

    TWEAK: user name to user login in email letter

--- woocommerce/emails/customer-reset-password.php
+++ woocommerce/emails/customer-reset-password.php
@@ -38,7 +38,7 @@ $user_name = $user->first_name ?? $user_login;
                         <tr>
                             <td style="font-family:'Open Sans',sans-serif;font-size:14px;line-height:200%;color:#3c5d90">
                                 <p><?php printf( esc_html__( 'Someone has requested a new password for the following account on %s:', 'woocommerce' ), esc_html( wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES ) ) ); ?></p>
-                                <p><?php printf( esc_html__( 'Username: %s', 'woocommerce' ), esc_html( $user_name ) ); ?></p>
+                                <p><?php printf( esc_html__( 'Username: %s', 'woocommerce' ), esc_html( $user_login ) ); ?></p>
                                 <p><?php esc_html_e( 'If you didn\'t make this request, just ignore this email. If you\'d like to proceed:', 'woocommerce' ); ?></p>
                                 <p>
                                     <a class="link" href="<?php echo esc_url( add_query_arg( array( 'key' => $reset_key, 'id' => $user_id ), wc_get_endpoint_url( 'lost-password', '', wc_get_page_permalink( 'myaccount' ) ) ) ); ?>"><?php ?>
