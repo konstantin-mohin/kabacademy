2a7748960a37fed99288cd247990652dacd5934d
Igor Chernenko <igorchernenko92@gmail.com>
2021-06-21 00:54:45 +0300

    ADD: three dots

--- test_r.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// $owner = $_POST['repository']['owner']['login'];
-// $req = file_get_contents("php://input");
-// $req = json_decode($req);
-$output = exec('./hooks/post-receive');
-var_dump($output);
