a1d45112b211185463db5af768eea5f183e4af79
Igor Chernenko <igorchernenko92@gmail.com>
2021-06-28 00:13:08 +0300

    ADD: donation fixes

--- donation_template.php
+++ donation_template.php
@@ -15,6 +15,7 @@ header('Expires: 0');
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Внести свой вклад</title>
@@ -39,16 +40,16 @@ header('Expires: 0');
 	<?php
 
 	global $post;
-    $products = get_field('donation',  $post->ID);
-    if ( $products ) {
+	$products = get_field('donation',  $post->ID);
+	if ($products) {
 
-    foreach ( $products as $item ) {
+		foreach ($products as $item) {
 			$id = $item->ID;
 			$title = sanitize_text_field($item->post_title);
-            $content = $item->post_content;
-            $content = apply_filters('the_content', $content);
-            $content = str_replace(']]>', ']]&gt;', $content);
-		    $product = wc_get_product( $id );
+			$content = $item->post_content;
+			$content = apply_filters('the_content', $content);
+			$content = str_replace(']]>', ']]&gt;', $content);
+			$product = wc_get_product($id);
 
 			?>
             <div class="row donation_raw">
@@ -61,27 +62,27 @@ header('Expires: 0');
 					<?php echo $content; ?>
 
                 </div>
-                <div class="col-md-5 clip">
+                <div class="col-md-5">
                     <div class="embed-responsive embed-responsive-16by9">
-						<?php echo  get_field( 'youtube_link',  $id); ?>
+						<?php echo  get_field('youtube_link',  $id); ?>
                     </div>
-					<?php if( $product->is_type( 'variable' ) ) { ?>
+					<?php if ($product->is_type('variable')) { ?>
 
 						<?php
 						$comment_args = array(
 							'comment_post_ID' => $id,
 						);
 
-						$comments = get_comments( array(
+						$comments = get_comments(array(
 							'post_id' => $id,
 							'orderby' => 'comment_date_gmt',
 							'status' => 'approve',
-						) ); ?>
+						)); ?>
 
                         <div class="comments_link">
                             <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M16.5565 11.7914C16.5565 13.2952 15.3339 14.5132 13.8244 14.5132H7.22161L3.74947 18.7659L4.77403 14.5132H4.20483C2.6953 14.5132 1.47266 13.2952 1.47266 11.7914V4.19318C1.47266 2.68942 2.6953 1.47144 4.20483 1.47144H13.8244C15.3339 1.47144 16.5565 2.68942 16.5565 4.19318V11.7914Z" stroke="#3C5D90" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                                <path d="M21.59 14.4224C21.59 15.6495 21.253 16.7994 20.6656 17.7838L21.7197 20.5668L18.6802 19.8772C17.626 20.5872 16.3556 21 14.9895 21C11.342 21 8.38672 18.0559 8.38672 14.4224C8.38672 10.7889 11.342 7.84485 14.9895 7.84485C18.6369 7.84485 21.59 10.7889 21.59 14.4224Z" stroke="#3C5D90" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M16.5565 11.7914C16.5565 13.2952 15.3339 14.5132 13.8244 14.5132H7.22161L3.74947 18.7659L4.77403 14.5132H4.20483C2.6953 14.5132 1.47266 13.2952 1.47266 11.7914V4.19318C1.47266 2.68942 2.6953 1.47144 4.20483 1.47144H13.8244C15.3339 1.47144 16.5565 2.68942 16.5565 4.19318V11.7914Z" stroke="#3C5D90" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M21.59 14.4224C21.59 15.6495 21.253 16.7994 20.6656 17.7838L21.7197 20.5668L18.6802 19.8772C17.626 20.5872 16.3556 21 14.9895 21C11.342 21 8.38672 18.0559 8.38672 14.4224C8.38672 10.7889 11.342 7.84485 14.9895 7.84485C18.6369 7.84485 21.59 10.7889 21.59 14.4224Z" stroke="#3C5D90" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                             <a data-fancybox data-src="#comment-modal-<?php echo $id; ?>" data-modal="true" href="javascript:;" class="comment_link"><?php echo count($comments) ?> комментариев </a>
                             </p>
@@ -95,15 +96,15 @@ header('Expires: 0');
                             </div>
 
                             <div class="donation_comments">
-                                <?php foreach( $comments as $comment ) { ?>
-                                     <div class="donation_comment_block">
+								<?php foreach ($comments as $comment) { ?>
+                                    <div class="donation_comment_block">
                                         <div class="comment_meta">
                                             <span class="comment_name"> <?php echo sanitize_text_field($comment->comment_author); ?> </span> <span class="comment_date"><?php echo date("j F Y", strtotime($comment->comment_date_gmt)); ?></span>
                                         </div>
 
                                         <div class="comment_content"><?php echo sanitize_text_field($comment->comment_content); ?></div>
-                                     </div>
-                                <?php } ?>
+                                    </div>
+								<?php } ?>
                             </div>
 
 
@@ -126,9 +127,21 @@ header('Expires: 0');
 
                         </div>
 
+					<?php } ?>
+                </div>
+                <div class="row progress-block">
+                    <div class="col-md-7 pb-4 pb-md-0">
+                            <span class="progress-block__title">
+                                Текущий взнос: <strong><?php echo do_shortcode('[wcj_product_total_orders_sum hide_currency="yes" product_id="' . $id . '"]') . ' / ' .  do_shortcode('[wcj_product_crowdfunding_goal hide_currency="yes" product_id="' . $id . '"]') ?> USD</strong>
+                            </span>
+                        <div class="custom_progress_bar">
+							<?php echo do_shortcode('[wcj_product_crowdfunding_goal_remaining_progress_bar product_id="' . $id . '"]'); ?>
+                        </div>
+                    </div>
+                    <div class="col-md-5">
                         <div class="modal-wrapper">
 
-                            <a class="btn btn-primary modal-button wlmodalgo">внести свой вклад</a>
+                            <a class="btn btn-primary progress-block__btn modal-button wlmodalgo">внести свой вклад</a>
                             <div class="donation-modal">
                                 <div class="modal-content">
                                     <span class="close">&times;</span>
@@ -151,18 +164,18 @@ header('Expires: 0');
                                         <input type="hidden" id="hidden_field" name="custom_price" class="custom_price" value="22">
                                         <div class="cart__payments cart-payments wlcart-payments2" id="tabs">
 											<?php
-											foreach ( $product->get_visible_children() as $variation_id ) {
-												$variation = wc_get_product( $variation_id ); ?>
+											foreach ($product->get_visible_children() as $variation_id) {
+												$variation = wc_get_product($variation_id); ?>
 
 												<?php
-												if ( $variation->price === '1' ) { ?>
-                                                    <label class="cart-payments__item cart-payment wlcart-payment2" id="tab-<?php echo $variation_id; ?>" >
+												if ($variation->price === '1') { ?>
+                                                    <label class="cart-payments__item cart-payment wlcart-payment2" id="tab-<?php echo $variation_id; ?>">
 
                                                         <input type="radio" class="cart-payment__input wlcart-payment__input" id="price-<?php echo $variation_id; ?>" name="variation_id" value="<?php echo $variation_id; ?>">
 
                                                         <div class="cart-payment__card wlcart-payment__card">
-<!--                                                            <div class="custom-text" style='margin-top: -12px; margin-bottom: 5px; font-size: 80%; '>другая:</div> -->
-                                                            <input type="text" id="price-custom" class="price-custom" name="price-custom" value="" placeholder="другая" >
+                                                            <!--                                                            <div class="custom-text" style='margin-top: -12px; margin-bottom: 5px; font-size: 80%; '>другая:</div> -->
+                                                            <input type="text" id="price-custom" class="price-custom" name="price-custom" value="" placeholder="другая">
                                                         </div>
 
                                                     </label>
@@ -171,10 +184,9 @@ header('Expires: 0');
 													continue;
 												} ?>
 
-                                                <label class="cart-payments__item cart-payment wlcart-payment2  <?php if ( $variation->price === '5' ) echo 'active'; ?>" id="tab-<?php echo $variation_id; ?>">
+                                                <label class="cart-payments__item cart-payment wlcart-payment2  <?php if ($variation->price === '5') echo 'active'; ?>" id="tab-<?php echo $variation_id; ?>">
 
-                                                    <input type="radio" class="cart-payment__input wlcart-payment__input" id="price-<?php echo $variation_id; ?>" name="variation_id" value="<?php echo $variation_id; ?>"
-														<?php if ( $variation->price === '5' ) echo 'checked'; ?>>
+                                                    <input type="radio" class="cart-payment__input wlcart-payment__input" id="price-<?php echo $variation_id; ?>" name="variation_id" value="<?php echo $variation_id; ?>" <?php if ($variation->price === '5') echo 'checked'; ?>>
 
                                                     <div class="cart-payment__card wlcart-payment__card">
                                                         <div class="cart-payment__title">$ <?php echo $variation->price ?? ''; ?></div>
@@ -194,20 +206,20 @@ header('Expires: 0');
                                             Информация о вкладчике
                                         </div>
 
-                                        <?php
-                                            if ( is_user_logged_in() ) {
-                                                $email = wp_get_current_user()->user_email;
-                                                $phone = get_user_meta(wp_get_current_user()->ID, 'billing_phone');
-                                                $city = get_user_meta(wp_get_current_user()->ID, 'billing_city');
-                                                $first_name = get_user_meta(wp_get_current_user()->ID, 'first_name');
-                                                $last_name = get_user_meta(wp_get_current_user()->ID, 'last_name');
-                                            }
+										<?php
+										if (is_user_logged_in()) {
+											$email = wp_get_current_user()->user_email;
+											$phone = get_user_meta(wp_get_current_user()->ID, 'billing_phone');
+											$city = get_user_meta(wp_get_current_user()->ID, 'billing_city');
+											$first_name = get_user_meta(wp_get_current_user()->ID, 'first_name');
+											$last_name = get_user_meta(wp_get_current_user()->ID, 'last_name');
+										}
 
-                                        ?>
+										?>
 
                                         <div class="cart__contacts wlcart__contacts">
                                             <label for="billing_first_name_donation-<?php echo $id; ?>" class="form-label">
-                                                <input type="" class = 'billing_first_name_donation wlbil first_name_donation' id="billing_first_name_donation-<?php echo $id; ?>" name="billing_first_name" value="<?php echo $first_name[0] ?? '' ?>">
+                                                <input type="" class='billing_first_name_donation wlbil first_name_donation' id="billing_first_name_donation-<?php echo $id; ?>" name="billing_first_name" value="<?php echo $first_name[0] ?? '' ?>">
                                                 <span>Имя</span>
                                             </label>
 
@@ -219,13 +231,13 @@ header('Expires: 0');
 
 
                                             <label for="billing_phone_donation-<?php echo $id; ?>" class="form-label">
-                                                <input type="tel" id="billing_phone_donation-<?php echo $id; ?>" class = 'billing_phone_donation wlbil' name="billing_phone" value="<?php echo $phone[0] ?? '' ?>">
+                                                <input type="tel" id="billing_phone_donation-<?php echo $id; ?>" class='billing_phone_donation wlbil' name="billing_phone" value="<?php echo $phone[0] ?? '' ?>">
                                                 <span>Телефон</span>
                                             </label>
 
 
                                             <label for="billing_email_donation-<?php echo $id; ?>" class="form-label">
-                                                <input type="email" id="billing_email_donation-<?php echo $id; ?>" class = 'billing_email_donation wlbil' name="billing_email" value="<?php echo $email ?? '' ?>">
+                                                <input type="email" id="billing_email_donation-<?php echo $id; ?>" class='billing_email_donation wlbil' name="billing_email" value="<?php echo $email ?? '' ?>">
                                                 <span>Email</span>
                                             </label>
 
@@ -261,7 +273,7 @@ header('Expires: 0');
 
 
                                             <div class="wlbutton-wrapper">
-                                                <div class="btn btn-small button add-to form wlsubmit" style= 'margin-top: 10px;'>Отправить</div>
+                                                <div class="btn btn-small button add-to form wlsubmit" style='margin-top: 10px;'>Отправить</div>
                                                 <div class="wlwarninglab"><span>!</span>Не заполнены обязательные поля</div>
                                             </div>
 
@@ -273,13 +285,6 @@ header('Expires: 0');
                                 </div>
                             </div>
                         </div>
-					<?php } ?>
-                </div>
-                <div class="col-7 progress-block">
-
-                    Текущий взнос: <strong><?php echo do_shortcode('[wcj_product_total_orders_sum hide_currency="yes" product_id="' . $id . '"]') . ' / ' .  do_shortcode('[wcj_product_crowdfunding_goal hide_currency="yes" product_id="' . $id . '"]') ?> USD</strong>
-                    <div class="custom_progress_bar">
-						<?php echo do_shortcode('[wcj_product_crowdfunding_goal_remaining_progress_bar product_id="' . $id . '"]'); ?>
                     </div>
                 </div>
             </div>
@@ -312,11 +317,11 @@ header('Expires: 0');
 <?php wp_footer(); ?>
 <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 </body>
+
 </html>
 
 <script>
-
-    jQuery( document ).on( 'click', '.modal-button',  function( e ) {
+    jQuery(document).on('click', '.modal-button', function(e) {
         jQuery(this).parents('.modal-wrapper').find('.donation-modal').show();
 
         jQuery(this).parents('.modal-wrapper').find('.donation-modal').find('.price-wrapper').removeClass('active');
@@ -337,29 +342,24 @@ header('Expires: 0');
     });
 
 
-    jQuery( document ).on( 'click', '.close',  function( e ) {
+    jQuery(document).on('click', '.close', function(e) {
         jQuery('.donation-modal').hide();
     });
 
-    jQuery( document ).on( 'click', '.price-wrapper',  function( e ) {
+    jQuery(document).on('click', '.price-wrapper', function(e) {
         jQuery('.price-wrapper').removeClass('active');
         jQuery(this).addClass('active');
     });
 
-    jQuery( document ).on( 'selectstart', '.variation-price-text p',  function( e ) {
+    jQuery(document).on('selectstart', '.variation-price-text p', function(e) {
         e.preventDefault();
     });
 
-    jQuery( document ).on( 'click', '.price-custom',  function( e ) {
+    jQuery(document).on('click', '.price-custom', function(e) {
         jQuery(this).parents('.price-wrapper').find('.variation-price').prop("checked", true);
     });
 
-    jQuery( document ).on( 'keyup', '.price-custom',  function( e ) {
-        this.value = this.value.replace(/\D/g,'');
+    jQuery(document).on('keyup', '.price-custom', function(e) {
+        this.value = this.value.replace(/\D/g, '');
     });
-
 </script>
-
-
-
-
